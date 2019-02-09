fe2faf08197d7541441b88e411626ed504692b3c
kai <kli@rufuslabs.com>
2019-02-09 15:54:22 -0800

    views public/add ignore

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 var date = new Date();
@@ -6,12 +7,25 @@ var date = new Date();
 var path=process.cwd();
 
 app.use(bodyParser.json());
+app.use(express.static("views"));
 
 app.get('/',function(req,res){
     console.log(req.url);
-    res.sendFile(path+'/views/main.html');
+    console.log("hahahha");
+    res.sendFile('/views/main.html', {root: __dirname });
 })
 
+app.get('/home',function(req,res){
+    console.log(req.url);
+    console.log("home");
+    res.sendFile('/views/index.html', {root: __dirname });
+})
+
+app.get('/entry',function(req,res){
+    console.log(req.url);
+    console.log("hahahha");
+    res.sendFile('/views/main.html', {root: __dirname });
+})
 
 http.listen(8080, function(){
   console.log('listening on :' + 8080);
